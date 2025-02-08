196da1b4f260d851287951dfa9634ad0c1843be9
Evaristo <114101006+EvaristoAguirre@users.noreply.github.com>
2025-02-08 12:01:29 -0300

    ADD/config comandera

--- backend/src/Printer/printer.controller.ts
+++ backend/src/Printer/printer.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { PrinterService } from './printer.service';
 
 @Controller('printing')
 export class PrinterController {
   constructor(private readonly printerService: PrinterService) {}
+
+  @Post()
+  async printTest() {
+    return this.printerService.printerOrder();
+  }
 }
--- backend/src/Printer/printer.service.ts
+++ backend/src/Printer/printer.service.ts
@@ -1,55 +1,69 @@
 import { Injectable } from '@nestjs/common';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import Printer from 'node-thermal-printer';
-
+// import Printer from 'node-thermal-printer';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const ThermalPrinter = require('node-thermal-printer').printer;
 @Injectable()
 export class PrinterService {
-  //   private printer: Printer;
-  //   constructor() {
-  //     // Configurar la impresora
-  //     this.printer = new Printer({
-  //       type: 'epson', // Tipo de impresora
-  //       interface: 'usb://192.168.0.100', // Dirección IP de la impresora
-  //       characterSet: 'SLOVENIA', // Configuración de caracteres (ajusta según tu modelo)
-  //     });
-  //     // Verificar conexión con la impresora
-  //     this.printer
-  //       .isPrinterConnected()
-  //       .then((connected) => {
-  //         console.log('Printer connected:', connected);
-  //       })
-  //       .catch((err) => {
-  //         console.error('Printer connection failed:', err);
-  //       });
-  //   }
-  //   async printerOrder(order: any): Promise<void> {
-  //     try {
-  //       this.printer.clear();
-  //       // Encabezado
-  //       this.printer.alignCenter();
-  //       this.printer.bold(true);
-  //       this.printer.println('Hansel & Gretel');
-  //       this.printer.println('--- COMANDA ---');
-  //       this.printer.newLine();
-  //       this.printer.bold(false);
-  //       // Información del pedido
-  //       this.printer.alignLeft();
-  //       this.printer.println(`Mesa: ${order.table}`);
-  //       this.printer.println(`Fecha: ${new Date().toLocaleString()}`);
-  //       this.printer.newLine();
-  //       // Detalles del pedido
-  //       this.printer.println('Detalles:');
-  //       order.items.forEach((item) => {
-  //         this.printer.println(`- ${item.quantity} x ${item.name}`);
-  //       });
-  //       this.printer.newLine();
-  //       this.printer.println('Gracias por su pedido.');
-  //       this.printer.cut();
-  //       // Enviar a la impresora
-  //       await this.printer.execute();
-  //       console.log('Comanda impresa exitosamente.');
-  //     } catch (err) {
-  //       console.error('Error al imprimir la comanda:', err);
-  //     }
-  //   }
+  private printer: any;
+  constructor() {
+    // Configurar la impresora
+    this.printer = new ThermalPrinter({
+      type: 'epson', // Tipo de impresora
+      interface: 'tcp://192.168.123.146', // Dirección IP de la impresora
+      characterSet: 'SLOVENIA', // Configuración de caracteres (ajusta según tu modelo)
+    });
+    // Verificar conexión con la impresora
+    this.printer
+      .isPrinterConnected()
+      .then((connected) => {
+        console.log('Printer connected:', connected);
+      })
+      .catch((err) => {
+        console.error('Printer connection failed:', err);
+      });
+  }
+  async printerOrder(): Promise<void> {
+    try {
+      this.printer.clear();
+      // Encabezado
+      this.printer.alignCenter();
+      this.printer.bold(true);
+      this.printer.println('Hansel & Gretel');
+      this.printer.println('--- COMANDA ---');
+      this.printer.newLine();
+      this.printer.newLine();
+      this.printer.newLine();
+      this.printer.newLine();
+      this.printer.bold(false);
+
+      // ---------------- Probando impresora -----------
+      this.printer.alignCenter();
+      this.printer.bold(true);
+      this.printer.println('Se vienen cositas...!!');
+      this.printer.newLine();
+      this.printer.newLine();
+      this.printer.bold(false);
+      this.printer.cut();
+      // Información del pedido
+      // this.printer.alignLeft();
+      // this.printer.println(`Mesa: ${order.table}`);
+      // this.printer.println(`Fecha: ${new Date().toLocaleString()}`);
+      // this.printer.newLine();
+
+      // Detalles del pedido
+      // this.printer.println('Detalles:');
+      // order.items.forEach((item) => {
+      //   this.printer.println(`- ${item.quantity} x ${item.name}`);
+      // });
+      // this.printer.newLine();
+      // this.printer.println('Gracias por su pedido.');
+      // this.printer.cut();
+
+      // Enviar a la impresora
+      await this.printer.execute();
+      console.log('Comanda impresa exitosamente.');
+    } catch (err) {
+      console.error('Error al imprimir la comanda:', err);
+    }
+  }
 }
