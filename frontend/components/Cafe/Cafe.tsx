d84a429fa732e2b7df378ea85bcf1fb02bf73e7d
samantadriuzzi <driuzzi.samanta.mayra@gmail.com>
2025-02-11 08:55:37 -0300

    pading top de café segun altura de navbar

--- frontend/components/Cafe/Cafe.tsx
+++ frontend/components/Cafe/Cafe.tsx
@@ -1,22 +1,30 @@
 "use client";
-import React, { useEffect } from "react";
-import { MesaInterface, MozoInterface } from "../Interfaces/Cafe_interfaces";
+import React, { useEffect, useState } from "react";
 import { useProductos } from "../Hooks/useProducts";
 import Salas from "../Salas/salas";
 
 const Cafe = () => {
   const { fetchAndSetProducts } = useProductos();
+  const [navbarHeight, setNavbarHeight] = useState(0);
+
 
   useEffect(() => {
     fetchAndSetProducts();
+
+    const navbar = document.querySelector("nav");
+    if (navbar) {
+      setNavbarHeight(navbar.offsetHeight);
+    }
   }, []);
 
   return (
-    <div className="cafe" style={{ display: "flex", height: "100vh", backgroundColor: "#D9CCBC", marginTop: "100px" }}>
+    <div className="cafe" style={{
+      display: "flex",
+      backgroundColor: "#D9CCBC", paddingTop: `${navbarHeight}px`
+    }}>
       <div style={{ width: "100%" }}>
         <Salas></Salas>
       </div>
-
     </div>
   );
 };
