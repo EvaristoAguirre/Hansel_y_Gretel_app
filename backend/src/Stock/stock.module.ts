3a6895faa60132f52b157d92cd677693ccc57d45
Evaristo <114101006+EvaristoAguirre@users.noreply.github.com>
2025-02-23 15:31:17 -0300

    FIX/dependencias en stock y entidades relacionadas

--- backend/src/Stock/stock.module.ts
+++ backend/src/Stock/stock.module.ts
@@ -1,11 +1,37 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { StockController } from './stock.controller';
 import { StockService } from './stock.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Stock } from './stock.entity';
+import { Product } from 'src/Product/product.entity';
+import { Ingredient } from 'src/Ingredient/ingredient.entity';
+import { ProductRepository } from 'src/Product/product.repository';
+import { IngredientRepository } from 'src/Ingredient/ingredient.repository';
+import { StockRepository } from './stock.repository';
+import { CategoryRepository } from 'src/Category/category.repository';
+import { CategoryModule } from 'src/Category/category.module';
+import { Category } from 'src/Category/category.entity';
+import { UnitOfMeasure } from 'src/Ingredient/unitOfMesure.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([
+      Stock,
+      Product,
+      Ingredient,
+      Category,
+      UnitOfMeasure,
+    ]),
+    forwardRef(() => CategoryModule),
+  ],
   controllers: [StockController],
-  providers: [StockService],
+  providers: [
+    StockService,
+    StockRepository,
+    ProductRepository,
+    IngredientRepository,
+    CategoryRepository,
+  ],
   exports: [],
 })
 export class StockModule {}
--- backend/src/Stock/stock.service.ts
+++ backend/src/Stock/stock.service.ts
@@ -2,31 +2,32 @@ import { Injectable } from '@nestjs/common';
 import { Stock } from './stock.entity';
 import { CreateStockDto } from 'src/DTOs/create-stock.dto';
 import { UpdateStockDto } from 'src/DTOs/update-stock.dto';
+import { StockRepository } from './stock.repository';
 
 @Injectable()
 export class StockService {
-  constructor(private readonly stockService: StockService) {}
+  constructor(private readonly stockRepository: StockRepository) {}
 
   async getAllStocks(page: number, limit: number): Promise<Stock[]> {
-    return await this.stockService.getAllStocks(page, limit);
+    return await this.stockRepository.getAllStocks(page, limit);
   }
 
   async getStockByProductId(productId: string): Promise<Stock> {
-    return await this.stockService.getStockByProductId(productId);
+    return await this.stockRepository.getStockByProductId(productId);
   }
 
   async getStockByIngredientId(ingredientId: string): Promise<Stock> {
-    return await this.stockService.getStockByIngredientId(ingredientId);
+    return await this.stockRepository.getStockByIngredientId(ingredientId);
   }
 
   async createStock(createStockDto: CreateStockDto): Promise<Stock> {
-    return await this.stockService.createStock(createStockDto);
+    return await this.stockRepository.createStock(createStockDto);
   }
 
   async updateStock(
     id: string,
     updateStockDto: UpdateStockDto,
   ): Promise<Stock> {
-    return await this.stockService.updateStock(id, updateStockDto);
+    return await this.stockRepository.updateStock(id, updateStockDto);
   }
 }
