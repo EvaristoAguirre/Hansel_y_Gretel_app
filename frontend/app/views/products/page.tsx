f6c1992efa7a3d754282b5cd3e734ffb2b8fef7d
samantadriuzzi <driuzzi.samanta.mayra@gmail.com>
2025-02-26 14:30:20 -0300

    context de ingredienes: listo

--- frontend/app/views/products/page.tsx
+++ frontend/app/views/products/page.tsx
@@ -1,12 +1,13 @@
+import IngredientsProvider from "@/app/context/ingredientsContext";
 import ProductsPage from "@/components/Products/ProductsPage";
 import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute";
 
 const ViewProducts = () => {
   return (
     <ProtectedRoute>
-
-      <ProductsPage />
-
+      <IngredientsProvider>
+        <ProductsPage />
+      </IngredientsProvider>
     </ProtectedRoute>
   );
 };
