bc406b24867ccae32bc9c8e1b2257da064d81142
samantadriuzzi <driuzzi.samanta.mayra@gmail.com>
2025-03-12 15:03:38 -0300

    .

--- frontend/components/Table/TableCard.tsx
+++ frontend/components/Table/TableCard.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MesaCardProps, MesaInterface } from "../Interfaces/Cafe_interfaces";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
