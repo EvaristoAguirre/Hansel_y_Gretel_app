5888b27e58db1e6de976bf455c14ccadd886a4d7
julieta548 <julietasosa548@gmail.com>
2025-03-21 12:02:06 -0300

    cambio en URI

--- frontend/components/URI/URI.js
+++ frontend/components/URI/URI.js
@@ -1,40 +1,40 @@
 //CATEGORIES
 
-export const URI_CATEGORY = "http://192.168.1.44:3000/category";
+export const URI_CATEGORY = "http://192.168.100.133:3000/category";
 
 //PRODUCTS
 
-export const URI_PRODUCT = "http://192.168.1.44:3000/product"
+export const URI_PRODUCT = "http://192.168.100.133:3000/product"
 
-export const URI_PRODUCT_BY_CATEGORY = "http://192.168.1.44:3000/product/by-categories"
+export const URI_PRODUCT_BY_CATEGORY = "http://192.168.100.133:3000/product/by-categories"
 
 
 //INGREDIENTS
 
-export const URI_INGREDIENT = "http://192.168.1.44:3000/ingredient";
+export const URI_INGREDIENT = "http://192.168.100.133:3000/ingredient";
 
 // UNITS OF MEASURE
 
-export const URI_UNIT_OF_MEASURE = "http://192.168.1.44:3000/ingredient/unitofmeasure";
+export const URI_UNIT_OF_MEASURE = "http://192.168.100.133:3000/ingredient/unitofmeasure";
 
 //ROOMS
 
-export const URI_ROOM = "http://192.168.1.44:3000/room"
+export const URI_ROOM = "http://192.168.100.133:3000/room"
 
 
 //TABLES
 
-export const URI_TABLE = "http://192.168.1.44:3000/tables"
+export const URI_TABLE = "http://192.168.100.133:3000/tables"
 
 //ORDERS
 
-export const URI_ORDER_OPEN = "http://192.168.1.44:3000/order/open"
+export const URI_ORDER_OPEN = "http://192.168.100.133:3000/order/open"
 
-export const URI_ORDER = "http://192.168.1.44:3000/order"
+export const URI_ORDER = "http://192.168.100.133:3000/order"
 
 //USERS
 
-export const URI_USER = "http://192.168.1.44:3000/user"
+export const URI_USER = "http://192.168.100.133:3000/user"
 
 //STOCK
 
