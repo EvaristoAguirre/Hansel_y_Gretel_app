906e23c752c7a106f4bf554548efc995b6d4ee9e
julieta548 <julietasosa548@gmail.com>
2025-03-26 18:14:42 -0300

    actualización script de backup

--- backup_postgres.bat
+++ backup_postgres.bat
@@ -4,7 +4,7 @@ set PGPASSWORD=200500123
 set PGHOST=localhost
 set PGPORT=5432
 set PGDATABASE=hansel_y_gretel
-set BACKUP_DIR="C:\Users\User\DriveBackupsHyG"
+set BACKUP_DIR="C:\Users\hanse\Backups Hansel y Gretel sql"
 
 :: Nombre de la tabla a exportar
 set TABLE_NAME=orders
@@ -30,7 +30,7 @@ echo "Archivo de backup: %BACKUP_FILE%" >> %LOG_FILE%
 :: Ejecutar pg_dump con la ruta corregida
 ::"C:\Program Files\PostgreSQL\16\bin\pg_dump.exe" -U %PGUSER% -h %PGHOST% -p %PGPORT% -F c -b -v -f "%BACKUP_FILE%" -t %TABLE_NAME% >> %LOG_FILE% 2>&1
 ::Backup filtrado según intervalo deseado
-"C:\Program Files\PostgreSQL\16\bin\psql.exe" -U %PGUSER% -h %PGHOST% -p %PGPORT% -d %PGDATABASE% -c "SELECT * FROM %TABLE_NAME% WHERE date >= (CURRENT_DATE - INTERVAL '14 days') AND date < (CURRENT_DATE - INTERVAL '7 days');" -o "%BACKUP_FILE%"
+"C:\Program Files\PostgreSQL\17\bin\psql.exe" -U %PGUSER% -h %PGHOST% -p %PGPORT% -d %PGDATABASE% -c "SELECT * FROM %TABLE_NAME% WHERE date >= (CURRENT_DATE - INTERVAL '14 days') AND date < (CURRENT_DATE - INTERVAL '7 days');" -o "%BACKUP_FILE%"
 
 if %errorlevel% neq 0 (
     echo "Error al crear el backup." >> %LOG_FILE%
@@ -42,7 +42,7 @@ echo Backup creado: %BACKUP_FILE%
 
 :: Exportar SOLO los registros de la semana anterior (7 días atrás)
 echo "Exportando tabla %TABLE_NAME% a CSV..." >> %LOG_FILE%
-"C:\Program Files\PostgreSQL\16\bin\psql.exe" -U %PGUSER% -h %PGHOST% -p %PGPORT% -d %PGDATABASE% -c "\copy (SELECT * FROM %TABLE_NAME% WHERE date >= (CURRENT_DATE - INTERVAL '14 days') AND date < (CURRENT_DATE - INTERVAL '7 days')) TO '%CSV_FILE%' WITH CSV HEADER;" >> %LOG_FILE% 2>&1
+"C:\Program Files\PostgreSQL\17\bin\psql.exe" -U %PGUSER% -h %PGHOST% -p %PGPORT% -d %PGDATABASE% -c "\copy (SELECT * FROM %TABLE_NAME% WHERE date >= (CURRENT_DATE - INTERVAL '14 days') AND date < (CURRENT_DATE - INTERVAL '7 days')) TO '%CSV_FILE%' WITH CSV HEADER;" >> %LOG_FILE% 2>&1
 
 if %errorlevel% neq 0 (
     echo "Error al exportar datos a CSV." >> %LOG_FILE%
@@ -54,6 +54,6 @@ echo CSV exportado correctamente: %CSV_FILE%
 
 :: Mover CSV a Google Drive
 echo "Moviendo archivo a: G:\Mi unidad\BackupsHyG\" >> %LOG_FILE%
-move "%CSV_FILE%" "G:\Mi unidad\BackupsHyG\"
+move "%CSV_FILE%" "G:\Mi unidad\Backups Hansel y Gretel"
 
 pause
