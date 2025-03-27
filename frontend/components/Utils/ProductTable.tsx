42f7220bf662502711877d74bd885f9089438f3d
samantadriuzzi <driuzzi.samanta.mayra@gmail.com>
2025-03-27 10:19:09 -0300

    Se hacen ajustes de estilos y del componente loading para que se adapte a todos los contenedores donde se llame. Se ajustan las vistasy componentes que ejecutan llamadas a la api para cargen el loading mientras se carga la data

--- frontend/components/Utils/ProductTable.tsx
+++ frontend/components/Utils/ProductTable.tsx
@@ -4,6 +4,7 @@ import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { esES } from "@mui/x-data-grid/locales";
 import { IRowData } from "../Interfaces/IGridMUI";
 import { capitalizeFirstLetterTable } from "./CapitalizeFirstLetter";
+import LoadingLottie from "../Loader/Loading";
 
 interface ProductTableProps {
   rows: IRowData[];
@@ -13,20 +14,25 @@ interface ProductTableProps {
 const DataGridComponent: React.FC<ProductTableProps> = ({ rows, columns, height = 450 }) => {
   return (
     <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
-      <DataGrid
-        rows={capitalizeFirstLetterTable(rows, ['name', 'description'])}
-        columns={columns}
-        localeText={esES.components.MuiDataGrid.defaultProps.localeText}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 1, pageSize: 5 },
-          },
-          sorting: {
-            sortModel: [{ field: "name", sort: "asc" }],
-          },
-        }}
-        pageSizeOptions={[2, 5, 7, 9, 15]}
-      />
+      {
+        rows.length === 0 ? (
+          <LoadingLottie />
+        ) :
+          <DataGrid
+            rows={capitalizeFirstLetterTable(rows, ['name', 'description'])}
+            columns={columns}
+            localeText={esES.components.MuiDataGrid.defaultProps.localeText}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 1, pageSize: 5 },
+              },
+              sorting: {
+                sortModel: [{ field: "name", sort: "asc" }],
+              },
+            }}
+            pageSizeOptions={[2, 5, 7, 9, 15]}
+          />
+      }
     </Box>
   );
 };
