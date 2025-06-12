ddf5c3141a7ab6c19ab44503d7338e14455c3f0b
samantadriuzzi <driuzzi.samanta.mayra@gmail.com>
2025-06-12 11:43:39 -0300

    ajuste en la vista de ingred para sumar a los grupos

--- frontend/components/Products/TabIngredients/Ingredients.tsx
+++ frontend/components/Products/TabIngredients/Ingredients.tsx
@@ -1,16 +1,27 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useIngredientsContext } from "@/app/context/ingredientsContext";
 import { useUnitContext } from "@/app/context/unitOfMeasureContext";
 import { FormType } from "@/components/Enums/Ingredients";
-import LoadingLottie from "@/components/Loader/Loading";
 import DataGridComponent from "@/components/Utils/ProductTable";
 import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Button, Box } from "@mui/material";
+import {
+  Button,
+  Box,
+  Tabs,
+  Tab,
+} from "@mui/material";
 import { GridCellParams } from "@mui/x-data-grid";
 import { FormIngredient } from "./FormIngredient";
+import ToppingsGroupList from "./Toppings/ToppingsGroupList";
+
+function TabPanel({ children, value, index }: any) {
+  return value === index ? <Box mt={2}>{children}</Box> : null;
+}
 
 const Ingredients = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+
   const {
     formType,
     formOpen,
@@ -80,31 +91,46 @@ const Ingredients = () => {
 
   return (
     <Box sx={{ m: 2, minHeight: "100vh" }}>
-      <Box display="flex" justifyContent="space-between" mb={2}>
-        <Button
-          variant="contained"
-          color="primary"
-          sx={{ width: "25%", height: 56 }}
-          onClick={() => openForm(FormType.CREATE)}
-          disabled={units.length === 0}
-        >
-          + Nuevo Ingrediente
-        </Button>
-      </Box>
+      <Tabs value={tabIndex} onChange={(_, newValue) => setTabIndex(newValue)}>
+        <Tab label="Lista de Ingredientes" />
+        <Tab label="Grupos de Agregados" />
+      </Tabs>
 
-      <DataGridComponent
-        rows={ingredients}
-        columns={columns}
-        capitalize={["name", "description"]}
-      />
+      <TabPanel value={tabIndex} index={0}>
+        <Box display="flex" justifyContent="space-between" mb={2}>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ width: "25%", height: 56 }}
+            onClick={() => openForm(FormType.CREATE)}
+            disabled={units.length === 0}
+          >
+            + Nuevo Ingrediente
+          </Button>
+        </Box>
 
-      {formOpen && (
-        <FormIngredient
-          units={units}
-          formType={formType}
-          onSave={formType === FormType.CREATE ? handleCreateIngredient : handleEditIngredient}
+        <DataGridComponent
+          rows={ingredients}
+          columns={columns}
+          capitalize={["name", "description"]}
         />
-      )}
+
+        {formOpen && (
+          <FormIngredient
+            units={units}
+            formType={formType}
+            onSave={
+              formType === FormType.CREATE
+                ? handleCreateIngredient
+                : handleEditIngredient
+            }
+          />
+        )}
+      </TabPanel>
+
+      <TabPanel value={tabIndex} index={1}>
+        <ToppingsGroupList />
+      </TabPanel>
     </Box>
   );
 };
