46d7e12c5c1a120ddd840ee10f1e1ff34428f62c
samantadriuzzi <driuzzi.samanta.mayra@gmail.com>
2025-06-13 11:22:16 -0300

    ajusto nombre dinámico a la descarga

--- frontend/api/exportPDF.ts
+++ frontend/api/exportPDF.ts
@@ -13,10 +13,21 @@ export const exportPDF = async (token: string) => {
 
     const blob = await response.blob();
 
+    // Obtenemos fecha y hora actuales
+    const now = new Date();
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const year = now.getFullYear();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+
+    const filename = `stock_${day}${month}${year}_${hours}${minutes}.pdf`;
+
+    // Link y disparardor de descarga
     const url = window.URL.createObjectURL(blob);
     const link = document.createElement('a');
     link.href = url;
-    link.setAttribute('download', 'stock.pdf');
+    link.setAttribute('download', filename);
     document.body.appendChild(link);
     link.click();
     link.remove();
