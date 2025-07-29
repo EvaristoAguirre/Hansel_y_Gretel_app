18d68b5b19e0dc16818bcc83160633adf4d3e979
samantadriuzzi <driuzzi.samanta.mayra@gmail.com>
2025-07-29 10:37:24 -0300

    nuevas variables de entornos

--- frontend/components/URI/URI.js
+++ frontend/components/URI/URI.js
@@ -1,126 +1,57 @@
-// //CATEGORIES
 
-// export const URI_CATEGORY = "http://192.168.0.50:3000/category";
+// EN PRODUCCION/CAFETERIA 
+// REEMPLAZAR:
+// 'API_URL_DESARROLLO' 
+// POR 'API_URL'
+// y NEXT_PUBLIC_API_URL_DESARROLLO por NEXT_PUBLIC_API_URL
 
-// //PRODUCTS
+const API_URL_DESARROLLO = process.env.NEXT_PUBLIC_API_URL_DESARROLLO;
 
-// export const URI_PRODUCT = "http://192.168.0.50:3000/product";
+if (!API_URL_DESARROLLO) {
+  throw new Error("Falta la variable de entorno NEXT_PUBLIC_API_URL");
+}
 
-// export const URI_PRODUCT_BY_CATEGORY =
-//   "http://192.168.0.50:3000/product/by-categories";
+// CATEGORIES
+export const URI_CATEGORY = `${API_URL_DESARROLLO}/category`;
 
-// //INGREDIENTS
+// PRODUCTS
+export const URI_PRODUCT = `${API_URL_DESARROLLO}/product`;
+export const URI_PRODUCT_BY_CATEGORY = `${API_URL_DESARROLLO}/product/by-categories`;
 
-// export const URI_INGREDIENT = "http://192.168.0.50:3000/ingredient";
-
-// // TOPPINGS-GROUP
-// export const URI_TOPPINGS_GROUP = "http://192.168.0.50:3000/toppings-group";
-// export const URI_TOPPINGS = "http://192.168.0.50:3000/ingredient/toppings";
-
-// // UNITS OF MEASURE
-
-// export const URI_UNIT_OF_MEASURE = "http://192.168.0.50:3000/unitofmeasure";
-
-// //ROOMS
-
-// export const URI_ROOM = "http://192.168.0.50:3000/room";
-
-// //TABLES
-
-// export const URI_TABLE = "http://192.168.0.50:3000/tables";
-
-// //ORDERS
-
-// export const URI_ORDER_OPEN = "http://192.168.0.50:3000/order/open";
-
-// export const URI_ORDER = "http://192.168.0.50:3000/order";
-
-// //PRINTS TICKETS
-
-// export const URI_TICKET = "http://192.168.0.50:3000/printer/printTicket";
-
-// //USERS
-
-// export const URI_USER = "http://192.168.0.50:3000/user";
-
-// //STOCK
-
-// export const URI_STOCK = "http://192.168.0.50:3000/stock";
-
-// //PDF
-
-// export const URI_PDF = "http://192.168.0.50:3000/export/stock/pdf";
-
-// // DAILY CASH
-
-// export const URI_DAILY_CASH = "http://192.168.0.50:3000/daily-cash";
-
-// DAILY CASH
-
-// export const URI_DAILY_CASH = "http://192.168.0.50:3000/daily-cash";
-
-// //MÉTRICAS
-// export const URI_METRICS = "http://192.168.0.50:3000/daily-cash/metrics";
-
-// -------------------------------------------------------------------------
-
-//CATEGORIES
-
-export const URI_CATEGORY = "http://localhost:3000/category";
-
-//PRODUCTS
-
-export const URI_PRODUCT = "http://localhost:3000/product";
-
-export const URI_PRODUCT_BY_CATEGORY =
-  "http://localhost:3000/product/by-categories";
-
-//INGREDIENTS
-
-export const URI_INGREDIENT = "http://localhost:3000/ingredient";
+// INGREDIENTS
+export const URI_INGREDIENT = `${API_URL_DESARROLLO}/ingredient`;
 
 // TOPPINGS-GROUP
-export const URI_TOPPINGS_GROUP = "http://localhost:3000/toppings-group";
-export const URI_TOPPINGS = "http://localhost:3000/ingredient/toppings";
+export const URI_TOPPINGS_GROUP = `${API_URL_DESARROLLO}/toppings-group`;
+export const URI_TOPPINGS = `${API_URL_DESARROLLO}/ingredient/toppings`;
 
 // UNITS OF MEASURE
+export const URI_UNIT_OF_MEASURE = `${API_URL_DESARROLLO}/unitofmeasure`;
 
-export const URI_UNIT_OF_MEASURE = "http://localhost:3000/unitofmeasure";
+// ROOMS
+export const URI_ROOM = `${API_URL_DESARROLLO}/room`;
 
-//ROOMS
+// TABLES
+export const URI_TABLE = `${API_URL_DESARROLLO}/tables`;
 
-export const URI_ROOM = "http://localhost:3000/room";
+// ORDERS
+export const URI_ORDER_OPEN = `${API_URL_DESARROLLO}/order/open`;
+export const URI_ORDER = `${API_URL_DESARROLLO}/order`;
 
-//TABLES
+// PRINTS TICKETS
+export const URI_TICKET = `${API_URL_DESARROLLO}/printer/printTicket`;
 
-export const URI_TABLE = "http://localhost:3000/tables";
+// USERS
+export const URI_USER = `${API_URL_DESARROLLO}/user`;
 
-//ORDERS
+// STOCK
+export const URI_STOCK = `${API_URL_DESARROLLO}/stock`;
 
-export const URI_ORDER_OPEN = "http://localhost:3000/order/open";
-
-export const URI_ORDER = "http://localhost:3000/order";
-
-//PRINTS TICKETS
-
-export const URI_TICKET = "http://localhost:3000/printer/printTicket";
-
-//USERS
-
-export const URI_USER = "http://localhost:3000/user";
-
-//STOCK
-
-export const URI_STOCK = "http://localhost:3000/stock";
-
-//PDF
-
-export const URI_PDF = "http://localhost:3000/export/stock";
+// PDF
+export const URI_PDF = `${API_URL_DESARROLLO}/export/stock`;
 
 // DAILY CASH
+export const URI_DAILY_CASH = `${API_URL_DESARROLLO}/daily-cash`;
 
-export const URI_DAILY_CASH = "http://localhost:3000/daily-cash";
-
-//MÉTRICAS
-
-export const URI_METRICS = "http://localhost:3000/daily-cash/metrics";
+// MÉTRICAS
+export const URI_METRICS = `${API_URL_DESARROLLO}/daily-cash/metrics`;
